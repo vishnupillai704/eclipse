f1c8403d111566ffc3318d437ba452ceb4d8eb8f
vishnupillai704 <vishnupillai704@gmail.com>
2022-02-01 15:02:29 +0530

    new line added

--- class msg{.java
+++ class msg{.java
@@ -2,5 +2,6 @@ class msg{
     public static void main(String args[]){
         System.out.println("hi this is ");
         System.out.println("this is vishnu"); 
+        System.out.println("welcome")
     }
 }
